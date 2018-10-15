609b3595f49dca682bd1f824590b9fb440c986c5
Kelly Mears <code@kellymears.me>
2018-10-15 11:20:31 -0700

    pre production deploy

--- site/web/app/themes/motivo/app/setup.php
+++ site/web/app/themes/motivo/app/setup.php
@@ -28,10 +28,13 @@ add_action('after_setup_theme', function () {
      * @link https://roots.io/plugins/soil/
      */
     add_theme_support('soil-clean-up');
+    add_theme_support('soil-disable-trackbacks');
     add_theme_support('soil-jquery-cdn');
+    add_theme_support('soil-js-to-footer');
     add_theme_support('soil-nav-walker');
     add_theme_support('soil-nice-search');
     add_theme_support('soil-relative-urls');
+    add_theme_support('soil-google-analytics', 'UA-XXXXX-Y', 'wp_head');
 
     /**
      * Enable plugins to manage the document title
