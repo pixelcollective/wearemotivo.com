c42af3763a5d38d267311430092b070db565f2a7
Kelly Mears <code@kellymears.me>
2018-11-08 11:42:07 -0800

    tracking code

--- site/web/app/themes/motivo/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/motivo/resources/assets/scripts/routes/common.js
@@ -1,6 +1,13 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1075061,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
